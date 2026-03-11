8f5dfba3d1cc987bbef5504dc69f38480baa340b
angeldzz <pintodiazangel@gmail.com>
2026-03-11 09:19:22 +0100

    login en navbar con manaed

--- NetCoreSeguridadPersonalizada/Controllers/ManagedController.cs
+++ NetCoreSeguridadPersonalizada/Controllers/ManagedController.cs
@@ -48,5 +48,11 @@ namespace NetCoreSeguridadPersonalizada.Controllers
                 return View();
             }
         }
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
